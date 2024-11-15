84d5e48d5147bd33ff6f99ebc7188a914e74b92e
chanakaHetti <cdileep40@gmail.com>
2024-11-15 23:08:18 +0000

    Adding the path helper

---
+++ src/paths.ts
@@ -0,0 +1,16 @@
+const pasths = {
+  home() {
+    return '/';
+  },
+  topicShow(topicSlug: string) {
+    return `/topics/${topicSlug}`;
+  },
+  postCreate(topicSlug: string) {
+    return `/topics/${topicSlug}/posts/new`;
+  },
+  postShow(topicSlug: string, postId: string) {
+    return `/topics/${topicSlug}/posts/${postId}`;
+  },
+};
+
+export default pasths;

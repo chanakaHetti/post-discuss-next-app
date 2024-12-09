c39be6cdc8c93278c2537e5c1df0def11b0c33f6
chanakaHetti <cdileep40@gmail.com>
2024-12-09 10:01:42 +0000

    Adding the button loading effect using the useActionState hook

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -31,6 +31,9 @@ export async function createTopic(
   formState: CreateTopicFormState,
   formData: FormData
 ): Promise<CreateTopicFormState> {
+  // Artificial delay to check loading spinner
+  // await new Promise((resolve) => setTimeout(resolve, 2500));
+
   const result = createTopicSchema.safeParse({
     name: formData.get('name'),
     description: formData.get('description'),

92e21b3e9b3e9e8268402a05805f2cfafb707334
chanakaHetti <cdileep40@gmail.com>
2024-12-09 17:33:31 +0000

    Adding the validation and other errors on the server action and UI implementation

--- src/components/posts/post-create-form.tsx
+++ src/components/posts/post-create-form.tsx
@@ -1,20 +1,30 @@
+'use client';
+
+import { useActionState } from 'react';
 import {
   Button,
   Input,
   Popover,
   PopoverContent,
   PopoverTrigger,
+  Textarea,
 } from '@nextui-org/react';
+
+import * as actions from '@/actions';
 import FormButton from '../common/form-button';
 
 export default function PostCreateForm() {
+  const [formState, action, isPending] = useActionState(actions.createPost, {
+    errors: {},
+  });
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create a Post</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action="">
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Post</h3>
 
@@ -23,16 +33,26 @@ export default function PostCreateForm() {
               label="Title"
               labelPlacement="outside"
               placeholder="Title"
+              isInvalid={!!formState.errors.title}
+              errorMessage={!!formState.errors.title?.join(', ')}
             />
 
-            <Input
+            <Textarea
               name="content"
               label="Content"
               labelPlacement="outside"
               placeholder="Content"
+              isInvalid={!!formState.errors.content}
+              errorMessage={!!formState.errors.content?.join(', ')}
             />
 
-            <FormButton isLoading={false}>Create Post</FormButton>
+            {formState.errors._form ? (
+              <div className="p-2 bg-red-200 border-red-400 rounded">
+                {formState.errors._form?.join(', ')}{' '}
+              </div>
+            ) : null}
+
+            <FormButton isLoading={isPending}>Create Post</FormButton>
           </div>
         </form>
       </PopoverContent>
